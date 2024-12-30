1832c662fd4c436a1c2a67bb76919bc4fe979975
Rupam Kairi <rupamkairi@hotmail.com>
2024-12-30 11:02:29 +0530

    cron_key added

--- pages/api/rest.ts
+++ pages/api/rest.ts
@@ -2,9 +2,17 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
-  res
-    .status(200)
-    .json({ name: "John Doe", datetime: new Date().toISOString() });
+  const { cron_key } = req.query;
+  if (cron_key !== process.env.CRON_KEY) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  res.status(200).json({
+    name: "John Doe",
+    datetime: new Date().toISOString(),
+  });
+  return;
 }
