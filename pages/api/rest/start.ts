e1028dbe192dac883426f4d9497330aa6fd19c99
Rupam Kairi <rupamkairi@hotmail.com>
2024-12-30 12:22:31 +0530

    eslint- warning bypass

--- pages/api/rest/start.ts
+++ pages/api/rest/start.ts
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { cron_key } = req.query;
+  // const { cron_key } = req.query;
   // if (cron_key !== process.env.CRON_KEY) {
   //   res.status(401).json({ error: "Unauthorized" });
   //   return;
